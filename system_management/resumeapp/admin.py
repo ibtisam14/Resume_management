cac015c1a3b508ab2e66ab53555d7e7d4f58d22f
Muhammad-Hamza-Liaqat <mh408800@gmail.com>
2025-07-29 19:17:50 +0500

    refactor: implement custom admin interface for Resume model with enhanced display and filtering options

--- system_management/resumeapp/admin.py
+++ system_management/resumeapp/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Resume
 
-admin.site.register(Resume)
+@admin.register(Resume)
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_email', 'status', 'submitted_at')
+    list_filter = ('status', 'submitted_at')
+    search_fields = ('user__email',)
+
+    def user_email(self, obj):
+        return obj.user.email
+    user_email.short_description = 'User Email'
