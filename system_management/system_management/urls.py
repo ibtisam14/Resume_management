0638073c5c351e412a67c2f363d4a05ac664ce68
Muhammad-Hamza-Liaqat <mh408800@gmail.com>
2025-07-29 17:34:35 +0500

    Enhance Swagger integration: add custom schema generator and UI for Bearer token support

--- system_management/system_management/urls.py
+++ system_management/system_management/urls.py
@@ -1,21 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.conf import settings
+from django.urls import path, include
 from django.conf.urls.static import static
-from system_management.swagger import schema_view  # keep this if you're using drf-yasg
-
+from .swagger import swagger_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-
-    path('api/', include('resumeapp_account.urls')),       # Register/Login APIs
-    path('api/', include('resumeapp.urls')),   # Resume Submit/Review APIs
-
-
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/', include('resumeapp_account.urls')),
+    path('api/', include('resumeapp.urls')),
 ]
-
-# ✅ Media file serving in development
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += swagger_urlpatterns
