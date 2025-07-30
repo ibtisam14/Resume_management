22bf67abe11de8a061c5def733388c32129b70b0
ibtisam14 <ibtsamulhaq14@gmail.com>
2025-07-30 14:17:12 +0500

    resume pdf is shown in admin panel

--- system_management/resumeapp/admin.py
+++ system_management/resumeapp/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Resume
 
 @admin.register(Resume)
 class ResumeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user_email', 'status', 'submitted_at')
+
+    list_display = ('id', 'user_email', 'status', 'submitted_at', 'resume_file')
     list_filter = ('status', 'submitted_at')
+
     search_fields = ('user__email',)
+
 
     def user_email(self, obj):
         return obj.user.email
     user_email.short_description = 'User Email'
+
+    def resume_file(self, obj):
+        if obj.file:
+            return format_html("<a href='{}' target='_blank'>View PDF</a>", obj.file.url)
+        return "No file uploaded"
+    resume_file.short_description = "Resume File"
--- system_management/server/urls.py
+++ system_management/server/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 from django.conf.urls.static import static
 from .swagger import swagger_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('api/', include('account.urls')),
     path('api/', include('resumeapp.urls')),
 ]
 urlpatterns += swagger_urlpatterns
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
